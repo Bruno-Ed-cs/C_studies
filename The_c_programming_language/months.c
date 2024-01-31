9e18f9019bb6e8f2b654379f84983b8ce1c8c6de
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2024-01-31 20:55:10 -0300

    chapter 2 start

---
+++ The_c_programming_language/months.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+enum months {
+
+    JAN = 1,
+    FEB, 
+    MAR,
+    APR,
+    MAY,
+    JUL,
+    JUY,
+    AUG,
+    SEP,
+    OCT,
+    NOV,
+    DEC
+};
+
+int main(){
+
+    int month = APR;
+    printf("%d\n", month);
+    return 0;
+
+}
